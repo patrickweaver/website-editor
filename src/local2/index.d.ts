7a7dea9fb1a155bab87ecda7c0963421ec415832
Patrick Weaver <pwdev@weavermail.org>
2025-04-10 21:59:31 -0700

    Remove js version

--- src/local2/index.d.ts
+++
@@ -1,6 +0,0 @@
-/**
- * Enables local editing mode for the website editor
- * This function sets up event listeners and controls for editing various aspects of the page
- * including text, images, colors, layout, and metadata.
- */
-export function localEditingMode(): void;
--- src/local2/util/random.ts
+++
@@ -1,3 +0,0 @@
-export function getUniqueId(readableString: string = "element") {
-  return `${readableString}-${Math.random().toString(10).slice(2, 12)}`;
-}
