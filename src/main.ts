d51d542fefe8c035be1ece496d1623d9d616a68b
Patrick Weaver <pwdev@weavermail.org>
2026-03-23 21:38:44 -0700

    Add pathname to isLocal

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,8 @@ import { isDevEnv } from "./local/util/isDevEnv";
 /* Check for local file or test environment */
 const isLocal: boolean =
   window.location.protocol === "file:" ||
-  window.location.hostname === "doodles.patrickweaver.net" ||
+  (window.location.hostname === "doodles.patrickweaver.net" &&
+    window.location.pathname === "/one-pager/") ||
   isDevEnv() ||
   window.navigator.userAgent.includes("jsdom");
 
